de20060bc8dbc3a6a5926995ffae0d05f5ef3bb4
hyrious <hyrious@outlook.com>
2021-09-10 18:14:44 +0800

    wip

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all: make.js temp.html
 
 make.js: make.jsx
-	npx esbuild make.jsx --bundle --outfile=make.js --jsx-factory=jsx --jsx-fragment=Fragment --inject:./inject.js --sourcemap=inline --sources-content=false
+	esbuild make.jsx --bundle --outfile=make.js --jsx-factory=jsx --jsx-fragment=Fragment --inject:./inject.js --sourcemap=inline --sources-content=false
 
 temp.html: make.rb
 	ruby make.rb -q
--- make.jsx
+++ make.jsx
@@ -1,11 +1,54 @@
-import { createElement, schedule, patch } from 'million'
+import parser from "postcss-selector-parser"
+import { createElement, patch } from "million"
 
-const loading = <p id="info" class="pt-4 px-4">Loading...</p>
-const ready = <p id="info" class="pt-4 px-4">Ready.</p>
-
+const loading = <div><p id="info" class="pt-4 px-4">Loading...</p></div>
 let el = createElement(loading)
 document.body.append(el)
 
-schedule(() => {
-    patch(el, ready)
+let allRules = document.styleSheets[0].cssRules
+let index = 0
+
+const render = () => <div>
+    <div style={{
+        height: '2px',
+        background: 'currentColor',
+        width: (100 * index / allRules.length) + '%'
+    }} />
+    <p id="info" class="pt-4 px-4">{index}/{allRules.length}</p>
+</div>
+
+let raf = 0;
+function update() {
+    if (index < allRules.length) {
+        rule = allRules[index]
+        handle(rule)
+    }
+
+    index = Math.min(index + 1, allRules.length)
+    if (index === allRules.length) {
+        patch(el, render())
+        cancelAnimationFrame(raf)
+        return;
+    }
+
+    if (index % 123 === 0) {
+        raf = requestAnimationFrame(update)
+        patch(el, render())
+    } else {
+        update()
+    }
+}
+raf = requestAnimationFrame(update)
+
+let a = new Set()
+let processor = parser(selectors => {
+    selectors.walkPseudos(selector => {
+        a.add(selector.value)
+    });
 })
+function handle(rule) {
+    if (rule instanceof CSSStyleRule) {
+        const selector = processor.processSync(rule.selectorText)
+    }
+}
+window.f = () => a;
