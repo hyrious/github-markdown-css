b7b76ea5875d8b0f923ab92c81af37dff112b3ee
hyrious <hyrious@outlook.com>
2021-09-12 16:08:42 +0800

    wip

--- index.js
+++ index.js
@@ -1,8 +1,51 @@
 // this file run in node
+import { tmpdir } from "os"
+import { existsSync, readFileSync, writeFileSync } from "fs"
+import { basename, join } from "path"
+import { get } from "https"
+import createSocksProxyAgent from 'socks-proxy-agent'
 import esbuild from "esbuild"
 
-esbuild.build({
+const log = () => {}
+const agent = createSocksProxyAgent('socks://localhost:10808')
+
+const cachedGet = (url) => {
+  const filename = basename(url)
+  const path = join(tmpdir(), filename)
+  if (existsSync(path)) {
+    log('cache hit', filename)
+    return readFileSync(path, "utf8")
+  }
+  log('fetching', url)
+  return new Promise((resolve, rej) => {
+    get("https://github.com", { agent }, (res) => {
+      let c = []
+      res.on('data', d => c.push(d))
+      res.on('end', () => {
+        const data = Buffer.concat(c).toString()
+        writeFileSync(path, data)
+        resolve(data)
+      })
+      res.on("error", rej)
+    }).on("error", rej)
+  })
+}
+
+const getCSS = (async () => {
+  const body = await cachedGet('https://github.com')
+  const css = body.match(/(?<=href=")\S+\.css/g)
+  const all = await Promise.all(css.map(cachedGet)).then(all => all.join('\n'))
+  writeFileSync('dist/all.css', all)
+})();
+
+const buildJSX = esbuild.build({
   entryPoints: ["main.jsx"],
   bundle: true,
   outdir: 'dist',
+  sourcemap: 'inline',
+  charset: 'utf8',
+  minifySyntax: true
 })
+
+await getCSS
+await buildJSX
--- main.jsx
+++ main.jsx
@@ -1,1 +1,9 @@
 // this file run in browser
+import React from "react"
+import ReactDOM from "react-dom"
+
+function App() {
+    return <p className="px-4 pt-4">Hello, world!</p>
+}
+
+ReactDOM.render(<App />, document.querySelector('#app'))
