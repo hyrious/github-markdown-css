47e25e2cd301b781d6218a046a1e64e517cf33af
hyrious <hyrious@outlook.com>
2021-10-04 09:21:26 +0800

    select theme

--- make.rb
+++ make.rb
@@ -118,7 +118,7 @@ markdown_body = markdown_body.flat_map { |s|
     next []
   end
   selectors = s[...s.index('{')].split(?,).map { |t|
-    next '.markdown-body' if %w( :root html body ).include? t
+    next '.markdown-body' if %w( :root html body [data-color-mode] ).include? t
     t.start_with?('.markdown-body') ? t : ".markdown-body #{t}"
   }
   if selectors.size == 1 and selectors[0] == '.markdown-body'
@@ -128,29 +128,69 @@ markdown_body = markdown_body.flat_map { |s|
     [selectors.join(?,) + s[s.index('{')..]]
   end
 }
+# remove duplicates in root
+root = root.join(';').split(';').reverse.uniq { |e| e.split(':')[0] }.reverse.join(';')
 
 require 'stringio'
 io = StringIO.new
 # io.puts theme
-io.puts '.markdown-body{' + root.join(';') + '}'
+io.puts '.markdown-body{' + root + '}'
 io.puts File.read 'prefix.css'
 io.puts markdown_body
 raw = io.string
 
+#=======================================
+# raw = plain css with var(--color-...)
+# colors = [["light", [["--color-...", "#123456"]]]]
+
+colors = colors.to_h
+variables = raw.scan(/var\((.+?)\)/).flatten.uniq
+
 Dir.mkdir 'dist' unless Dir.exist? 'dist'
 test_html = cached_get 'https://sindresorhus.com/github-markdown-css/'
-test_html[/<head>/] = '<head><meta name="color-scheme" content="dark">'
-test_html[/<body>/] = '<body class="markdown-body">'
+test_html[/<head>/] = '<head><meta name="color-scheme" content="light dark">'
+test_html[/<body>/] = '<body class="markdown-body light">'
+options = colors.keys.map { |e| "<option value=\"#{e}\">#{e}</option>" }.join("\n")
+links = colors.keys.map { |e| "<link rel=\"stylesheet\" href=\"./#{e}.css\">" }.join("\n")
+schemes = colors.keys.map { |e| ".#{e}{color-scheme:#{e.split('_')[0]}}" }.join("\n")
+test_html[/<\/body>/] = <<~HTML
+  <script>
+    var lastColor = 'light'
+    function changeColor(select) {
+      var newColor = select.value
+      document.body.classList.replace(lastColor, newColor)
+      lastColor = newColor
+    }
+  </script>
+  <select style="position: fixed; top: 1em; right: 1em; font-size: 16px;"
+          onchange="changeColor(this)">
+  #{options}
+  </select>
+  #{links}
+  <style>
+  #{schemes}
+  </style>
+  </body>
+HTML
 test_html[/<a class="github-fork-ribbon".+/] = ''
 File.write 'dist/index.html', test_html
 
-light = colors[0][1].reverse
-dark = colors.find { |(name, _)| name == 'dark' }[1].reverse
-light = dark + light
-lighted = raw.gsub(/var\((.+?)\)/) { |t|
-  m = t.match /var\((.+?)\)/
-  var = m[1]
-  _, value = light.find { |(name, _)| name == var }
-  next value
+colors.each { |name, vars|
+  vars.select! { |(var, value)| variables.include? var }
+  body = vars.map { |(var, value)| "#{var}:#{value}" }.join(';')
+  File.write "dist/#{name}.css", ".#{name}{#{body}}\n"
 }
-File.write 'dist/github-markdown.css', lighted
+File.write 'dist/github-markdown.css', raw
+
+# light = colors[0][1].reverse
+# dark = colors.find { |(name, _)| name == 'dark' }[1].reverse
+# light = dark + light
+# lighted = raw.gsub(/var\((.+?)\)/) { |t|
+#   m = t.match /var\((.+?)\)/
+#   var = m[1]
+#   # there's no --color-text-primary
+#   var = '--color-fg-default' if var == '--color-text-primary'
+#   _, value = light.find { |(name, _)| name == var }
+#   value or t
+# }
+# File.write 'dist/github-markdown.css', raw
